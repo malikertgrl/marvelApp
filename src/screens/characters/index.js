da9def67f3b9ec00da09e40ce37dce8b7feae84f
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2022-01-14 17:10:26 +0300

    flatlist

--- src/screens/characters/index.js
+++ src/screens/characters/index.js
@@ -1,31 +1,78 @@
 import React, { useEffect } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native'
 import { api } from '../../api'
 import { useSelector, useDispatch } from "react-redux"
+import { set_characters } from '../../redux/actions'
 
 
 const Characters = () => {
-    const state = useSelector(state => state.SystemReducer)
+    const dispatch = useDispatch()
+    const { characters, loading } = useSelector(state => state.SystemReducer)
 
     const getItem = () => {
         // console.log("api", api)
         fetch(api)
             .then(response => response.json())
-            .then(data => console.log("eee"))
+            .then(data => dispatch(set_characters(data.data.results)))
             .catch(e => console.log(e))
     }
 
     useEffect(() => {
         getItem()
-        console.log(state)
+        console.log("karakterler", JSON.stringify(characters, null, 4))
+
     }, [])
+
     return (
         <View >
-            <FlatList />
+            <FlatList
+                data={characters}
+                ItemSeparatorComponent={() => {
+                    return (
+                        <View style={styles.seperatorStyle} />
+                    )
+                }}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={{ margin: 5 }}>
+                            <TouchableOpacity>
+                                <View style={{ alignItems: "center" }}>
+                                    <Text style={{ fontSize: 25 }}>{item.name}</Text>
+                                    <Image
+                                        style={{ width: 200, height: 200 }}
+                                        source={{
+                                            uri: `${item.thumbnail.path}.${item.thumbnail.extension}`,
+                                        }}
+                                    />
+                                </View>
+                                <View>
+
+                                    <Text>{item.description && `\n${item.description}`}</Text>
+                                    <Text>comics: {item.comics.returned}</Text>
+                                    <Text>series:{item.series.returned}</Text>
+                                    <Text>stories:{item.stories.returned}</Text>
+
+
+
+                                </View>
+                            </TouchableOpacity>
+
+
+
+
+                        </View>
+                    )
+                }}
+            />
+
+
+
         </View>
     )
 }
 
 export default Characters
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    seperatorStyle: { borderBottomWidth: 1, borderBottomColor: "gray" }
+})
