cd23917460dd38cf37d3997330a6b4bfdb07efc7
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2022-01-16 23:37:50 +0300

    bug fixed

--- src/navigation/CharacterStack.js
+++ src/navigation/CharacterStack.js
@@ -10,17 +10,21 @@ const CharacterStack = () => {
     const Stack = createStackNavigator();
 
     return (
-        <Stack.Navigator screenOptions={{
-            //gestureEnabled: true screenOptions içerisinde çalıştırmak lazım bir de
-            //gestureDirection ile yön belirtmek gerekiyor.
-            gestureDirection: 'horizontal',
-            gestureEnabled: true,
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-            // headerStyle: { backgroundColor: blueColor },
-            headerTitleAlign: "center",
-            // headerTintColor: "#fff"
+        <Stack.Navigator
+        //  screenOptions={{
+        //     //gestureEnabled: true screenOptions içerisinde çalıştırmak lazım bir de
+        //     //gestureDirection ile yön belirtmek gerekiyor.
+        //     gestureDirection: 'horizontal',
+        //     gestureEnabled: true,
+        //     cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        //     // headerStyle: { backgroundColor: blueColor },
+        //     headerTitleAlign: "center",
+        //     // headerTintColor: "#fff"
 
-        }} >
+        // }} 
+
+        // burayı açtığımda içerdeki horizontal flatlist bozuldu. 
+        >
             <Stack.Screen name="Characters" component={Characters} options={{ headerShown: false }} />
             <Stack.Screen name="CharacterDetails" component={CharacterDetails} options={{
                 title: 'Karakter Detayı'
